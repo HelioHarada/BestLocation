995f396b6176dddcf9f509dc2583d9179d9ac12b
Hélio Harada <helioharada30@gmail.com>
2018-11-01 12:02:02 -0200

    Listagem menu

--- dist/vue.js
+++ dist/vue.js
@@ -175,6 +175,58 @@ var loginModal = new Vue({
   })
 
 
+  // Listam Casa 
+
+  Vue.component('card-house',{
+    template: ` 
+    <div class="row">
+    
+      <div class="col-md-4 card-house">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="casa.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+
+      <hr>
+
+      <div class="col-md-4 card-house">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="casa.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 card-house">
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="casa.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+
+    </div>
+    `
+  })
+
+  var loginModal = new Vue({
+    el: '.cardHouse',
+  })
+  
+
+
   // export default{
   //   methods: {
   //     alterar(){
