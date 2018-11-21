be65ae4a710f83f61165bae5022dd834f15d451f
Leonardo Molina <leomolina0501@hotmail.com>
2018-11-21 21:43:47 -0200

    Subindo mas tem que arrumar

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 // importação módulo mongoose
 var mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/bdBestLocation'
+module.exports = mongoose.connect(url, { useNewUrlParser: true })
+
 // conexão no banco
-mongoose.connect('mongodb://localhost/bdBestLocation', { useNewUrlParser: true } );
+//mongoose.connect('mongodb://localhost/bdBestLocation', { useNewUrlParser: true } );
 
 // ouvindo eventos que ocorrem em sucesso ou erro de conexão
 mongoose.connection.on('connected', () => {
